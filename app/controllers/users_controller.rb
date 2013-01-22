cd03bba762668b7846144defe59e369caf45dcff
Charlie Bedard <charlie@cks-partners.com>
2013-01-22 15:37:56 -0800

    Added settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   # Got a POST request with entries set for the new user. Validate and use them if OK.
     @user = User.new(params[:user])
     if @user.save
-      # Before sending the user off to their profiole page, also sign them in...
+      # Before sending the user off to their profile page, also sign them in...
       sign_in(@user)
 
       # Here on successful. Already saved in the DB. Set the "Welcome message"...
@@ -36,8 +36,27 @@ class UsersController < ApplicationController
   end
 
   def edit
+  # Here to allow a user to edit his/her profile
+  	@user=User.find(params[:id])
   end
 
   def update
+  # Here to capture the changes from a user's profile edits
+		@user=User.find(params[:id])
+		if @user.update_attributes(params[:user])
+    	# Here on successfu update of profile. Acknowledge success...
+      flash[:success] = "Profile updated"
+      
+      # Before sending the user off to their profile page, also sign them in...
+      sign_in(@user)
+
+      # Now, redirect to the Profile page to display the updated user...
+      redirect_to @user
+
+    else
+      # Bad data values. Deliver error and retry...
+      render 'edit'
+    end
+  
   end
 end
